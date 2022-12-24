8d2f1e9c3db7fbad07872e053b09a1b2c79defcc
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2022-12-24 17:10:52 +0530

    build: :rocket: v0.0.1 stable release

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const redirectBtn = document.querySelector("#redirect-btn");
+
+redirectBtn.addEventListener("click", () => {
+    window.location.href = "https://playground.compile-me.com";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "fira-code": "'Fira Code', monospace"
+      },
+      colors: {
+        primaryDark: "#1C1B2B",
+        secondaryDark: "#10111A",
+        primaryLight: "#F4F6FA",
+        secondaryLight: "#F1F2F6",
+        primaryOrange: "#FC5830",
+        lightGrey: "#B5BAC7",
+        white: "#FFFFFF",
+        primaryYellow: "#F6BA3D",
+        semiTransparent: "rgba(0, 0, 0, 0.6)",
+        primaryGreen: "#08CD92",
+        primaryRed: "#D61C4E"
+      }
+    },
+  },
+  plugins: [],
+}
